cb201cf6951cbf7abb8a13669d1de7edbfbb7984
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2019-04-11 15:09:39 +0200

    interface Builder Demo

---
+++ IBDemo/IBDemo/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  IBDemo
+//
+//  Created by JETS Mobile Lab - 5 on 4/11/19.
+//  Copyright © 2019 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+- (IBAction)button:(UIButton *)sender;
+
+@end
+
---
+++ IBDemo/IBDemo/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  IBDemo
+//
+//  Created by JETS Mobile Lab - 5 on 4/11/19.
+//  Copyright © 2019 ios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)button:(UIButton *)sender {
+    [_label setText:@"hey amr"];
+}
+@end
